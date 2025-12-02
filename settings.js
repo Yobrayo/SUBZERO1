4e3a55410b5c0fd71262e2aba3d4980878750a33
Yobrayo <hitityobrayo@gmail.com>
2025-12-02 13:57:44 +0300

    Update SESSION_ID and OWNER_NUMBER in settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~QC1RK5", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254115548821", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
